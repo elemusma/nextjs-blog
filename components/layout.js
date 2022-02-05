eda3547a141c0140b4d2d1dd37d31a394bc76c95
Efrain Lemus <efraintlemusm@gmail.com>
2022-02-05 11:27:07 -0700

    added og image from layout js

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'T'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'First Next.js sample website with T'
 
 export default function Layout({ children, home }) {
   return (
@@ -16,12 +16,13 @@ export default function Layout({ children, home }) {
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        <meta
+        <meta property="og:image" content="/images/Downtown-Denver-Optimized.jpg"></meta>
+        {/* <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        /> */}
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
